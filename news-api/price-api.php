55b475f38801f61774bbb588479dcec1f78afe61
fatooo0116@gmail.com <cbsi940424>
2021-05-14 11:43:51 +0800

    update

--- news-api/price-api.php
+++ news-api/price-api.php
@@ -1,5 +1,14 @@
 <?php
 
+
+
+
+
+
+
+
+
+
 add_action( 'rest_api_init', function () {
     register_rest_route( 'cargo/v1', '/get_price', array(
       'methods' => 'POST',
@@ -9,28 +18,31 @@ add_action( 'rest_api_init', function () {
   function get_staff_price_handler($data){
     
     $cid = (isset($data['cid'])) ? $data['cid'] : 0; 
-
-    
-    // $page = (isset($data['page'])) ? $data['page'] : 0; 
-    // $post_per_page = (isset($data['post_per_page'])) ? $data['post_per_page'] : 0; 
+    $pid = (isset($data['pid'])) ? $data['pid'] : 0; 
   
     global $wpdb;
-    $table_name =  $wpdb->prefix . 'cprice';;
   
-    
-    $sql = "SELECT * FROM $table_name WHERE product_id=".$cid;
+    $table_price =  $wpdb->prefix . 'cprice';;
+  
+  
+  
+    $sql = "SELECT * From ".$table_price." WHERE product_id='".$pid."' AND customer_id='".$cid."'";
     // $sql .= ' order by product_id ASC';
-    
-    $results = $wpdb->get_results($sql);
+    $results = $wpdb->get_results($sql,ARRAY_A);
+  
+    // return $sql;
+
     if(!empty($results)){  
-        return $results;
-      // return  $page.' '.$post_per_page ;
-       // return $sql;
+      return $results;
+  
     }else{
       return 0;
-    }
+    } 
    
   }
+
+
+
 
 
 
@@ -39,9 +51,77 @@ add_action( 'rest_api_init', function () {
       'methods' => 'POST',
       'callback' => 'update_price_handler',
     ) );
-  }
-  function get_staff_price_handler($data){
+  });
+
+  function update_price_handler($data){
     $cid = (isset($data['cid'])) ? $data['cid'] : 0; 
-    $product_id = (isset($data['product_id'])) ? $data['product_id'] : 0; 
+    $pid = (isset($data['pid'])) ? $data['pid'] : 0; 
+    $price = (isset($data['price'])) ? $data['price'] : 0; 
+
+   //  $sql = "SELECT * FROM $table_name WHERE product_id=".$pid." AND customer_id=".$cid;
+  // $sql .= ' order by product_id ASC';
     
+  global $wpdb;
+
+    if($cid!=0 | $pid!=0){
+
+      $table_name =  $wpdb->prefix . 'cprice';;
+      $sql = "SELECT * FROM $table_name WHERE product_id=".$pid." AND customer_id=".$cid;
+      $count = $wpdb->get_var($sql);
+      if($count>0){
+        /*  Update */
+      $result =   $wpdb->update(
+                      $table_name,
+                      array(
+                        'price' => $price,                       
+                      ), 
+                      array(
+                        'product_id'=> $pid,
+                        'customer_id'=> $cid,
+                      ));
+
+      }else{
+        /* Insert */
+          $data =  array(
+          'product_id'=> $pid,
+          'price' => $price,      
+          'customer_id'=> $cid,
+        );     
+        $result = $wpdb->insert($table_name,$data);
+      }    
+      
+    }
+    
+   // $results = $wpdb->get_results($sql);
+   if($result){
+    return $price;
+   }else{
+    return 0;     
+   }
+
+   // return $result ;
+  }
+
+
+
+
+
+
+/* 
+*    get price by  
+     @ product id
+     @ customer id
+*/
+  function get_price_by_customer($data){
+    $cid = (isset($data['cid'])) ? $data['cid'] : 0; 
+   // $pid = (isset($data['pid'])) ? $data['pid'] : 0; 
+
+
+    if($cid!=0 | $pid!=0){
+      $sql = "SELECT * FROM $table_name WHERE  customer_id=".$cid;
+      $results = $wpdb->get_results($sql);
+      return  $results;
+    }else{
+      return 0;
+    }
   }
